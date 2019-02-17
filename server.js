76129a808755391bee956d6750862dd60fa446db
AmjadHasnawi <civilamjad227@gmail.com>
2019-02-17 14:40:20 +0200

    try to deploy

--- server.js
+++ server.js
@@ -31,14 +31,14 @@ db.once('open', function () {
 
 
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 // // Serve any static files
 app.use(express.static(path.join(__dirname, './client/build')));
 // // Handle React routing, return all requests to React app
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
-// }
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`))
